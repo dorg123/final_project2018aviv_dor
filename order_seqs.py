1b16273596d3be31ec310b9bb248abaf2474d974
dorg123 <dor.genosar@outlook.com>
2018-06-20 19:44:31 +0300

    changed the order and added len column to subseqs_ordered.txt

--- order_seqs.py
+++ order_seqs.py
@@ -1,7 +1,7 @@
 with open('subseqs.txt') as f:
-    lines = list(line.split('\t') for line in f.readlines())
-lines.sort(key=lambda x: (len(x[0]), int(x[1])), reverse=True)
+    lines = list((str(len(lst[0])), lst[1], lst[2], lst[0]) for lst in (line.rstrip('\n').split('\t') for line in f.readlines()))
+lines.sort(key=lambda x: int(x[1]), reverse=True)
 with open('subseqs_ordered.txt', 'w') as f:
     for line in lines:
-        f.write('\t'.join(line))
+        f.write('\t'.join(line) + '\n')
 print('done')
